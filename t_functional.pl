7ab649a27dd6b60cc4470e00edd7d78bd85f4b3e
David Bradford <dbradford@omniti.com>
2014-11-10 10:21:18 -0500

    add template files

---
+++ t_functional.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+# purpose: functional (vs class) template
+
+use strict;
+use warnings FATAL => 'all';
+
+use Data::Dumper;
+
+sub function1 {
+    print "I am function1\n";
+}
+
+sub main {
+    my @argv = @_;
+    print Dumper(\@argv);
+    function1();
+    return;
+}
+
+my $rc = ( main(@ARGV) || 0 );
+
+exit $rc;
+
