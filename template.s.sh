371ef7557825fb89973af6ada0ea9a80dd613ab1
David Bradford <dbradford@omniti.com>
2014-11-22 14:37:20 -0500

    add params and whitespace

--- template.s.sh
+++ template.s.sh
@@ -8,14 +8,26 @@ echo "replace this with code"
 
 # <- END_OF_CODE
 # <- $VAR1 = {
-# <-     VERSION => '0.0.6',
-# <-     PROG    => 'c',?
-# <-     purpose => 'Search for a file and add it to the current "p" project.',
-# <-     params  => 'FILENAME_PART',
-# <-     example => 'menu.h',
-# <-     exit    => 'return',?
-# <-     CODE    => $code,
+# <-                VERSION => '0.0.6',
+# <-                   PROG => 'c',?
+# <-                purpose => 'Search for a file and add it to the current "p" project.',
+# <-                 params => 'FILENAME_PART',
+# <-                example => 'menu.h',
+# <-                   exit => 'return',?
+# <-                   CODE => $code,
 # <-     top_comments_block => $top_comments_block,
-# <-     target  => "$ENV{HOME}/bin/af",
+# <-                 target => "$ENV{HOME}/bin/af",
+# <-     options => [
+# <-         {
+# <-             long_switch => 'backdate=i',
+# <-              short_desc => 'backdate=#DAYS',
+# <-               long_desc => 'edit # days ago instead of today',
+# <-         },
+# <-         {
+# <-             long_switch => 'yesterday',
+# <-              short_desc => 'yesterday',
+# <-               long_desc => "edit yesterday's data instead of today's",
+# <-         },
+# <-     ],
 # <- };
 
