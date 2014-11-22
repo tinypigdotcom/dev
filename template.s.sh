32b37442582fc849ee44a87f339e9df3ff0bb165
David Bradford <dbradford@omniti.com>
2014-11-22 14:15:06 -0500

    add more options: top_comments_block, PROG, exit

--- template.s.sh
+++ template.s.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# <- my $top_comments_block = <<'END_OF_COMMENTS';
+# These comments come first.
+# <- END_OF_COMMENTS
 # <- my $code = <<'END_OF_CODE';
 
 echo "replace this with code"
@@ -6,9 +9,13 @@ echo "replace this with code"
 # <- END_OF_CODE
 # <- $VAR1 = {
 # <-     VERSION => '0.0.6',
+# <-     PROG    => 'c',?
 # <-     purpose => 'Search for a file and add it to the current "p" project.',
 # <-     params  => 'FILENAME_PART',
 # <-     example => 'menu.h',
+# <-     exit    => 'return',?
 # <-     CODE    => $code,
+# <-     top_comments_block => $top_comments_block,
 # <-     target  => "$ENV{HOME}/bin/af",
 # <- };
+
