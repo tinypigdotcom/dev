29066138577bda95ea66fe4a9113f728625893aa
David Bradford <dbradford@omniti.com>
2014-11-23 13:23:15 -0500

    add nnl

---
+++ nnl.s.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# <- my $code = <<'END_OF_CODE';
+
+OUTFILE=$HOME/.nnotes
+less $OUTFILE
+
+# <- END_OF_CODE
+# <- $VAR1 = {
+# <-     VERSION => '0.0.2',
+# <-     purpose => 'note to self - show all',
+# <-     params  => '',
+# <-     example => '',
+# <-     CODE    => $code,
+# <-     target  => "$ENV{HOME}/bin/nnl",
+# <- };
+
