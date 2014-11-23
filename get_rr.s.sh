9039d3b9933228a46da659c04462d992a690d5e7
David Bradford <dbradford@omniti.com>
2014-11-23 12:19:08 -0500

    add get_rr

---
+++ get_rr.s.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# <- my $code = <<'END_OF_CODE';
+
+cat ~/.rr
+
+# <- END_OF_CODE
+# <- $VAR1 = {
+# <-     VERSION => '0.0.2',
+# <-     purpose => 'works with "rr" to allow easily pulling snippets into vim',
+# <-     params  => '',
+# <-     example => '',
+# <-     CODE    => $code,
+# <-     target  => "$ENV{HOME}/bin/get_rr",
+# <- };
+
