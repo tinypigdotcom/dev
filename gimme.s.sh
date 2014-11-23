e97852292a5901da199604309689dd0a51c640c2
David Bradford <dbradford@omniti.com>
2014-11-23 12:22:39 -0500

    add gimme

---
+++ gimme.s.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# <- my $code = <<'END_OF_CODE';
+
+set -x
+cp ~/.vim/_vimrc ~/.vimrc
+cp ~/utility/_bash_profile ~/.bash_profile
+
+# <- END_OF_CODE
+# <- $VAR1 = {
+# <-     VERSION => '0.0.2',
+# <-     purpose => 'to update local .vimrc and .bash_profile',
+# <-     params  => '',
+# <-     example => '',
+# <-     CODE    => $code,
+# <-     target  => "$ENV{HOME}/bin/gimme",
+# <- };
+
