87923df357c8afc42e2c37fe113ada901a95c55b
David Bradford <dbradford@omniti.com>
2014-11-24 00:00:05 -0500

    add v

---
+++ v.s.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# <- my $code = <<'END_OF_CODE';
+
+source ~/.penv
+
+# <- END_OF_CODE
+# <- $VAR1 = {
+# <-     VERSION => '0.0.2',
+# <-     PROG    => 'v',
+# <-     purpose => 'get variables from "p" project files',
+# <-     params  => '',
+# <-     example => '',
+# <-     exit    => 'return',
+# <-     CODE    => $code,
+# <-     target  => "$ENV{HOME}/bin/v",
+# <- };
+
