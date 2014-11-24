06a4bf18f1d978c3aca7136d916ba7c06af63956
David Bradford <dbradford@omniti.com>
2014-11-24 00:11:10 -0500

    add xn

---
+++ xn.s.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# <- my $code = <<'END_OF_CODE';
+
+set -x
+
+INBOX_PATH=/cygdrive/c/Dropbox/dmb/email
+OUTFILE="$INBOX_PATH/$*"
+
+echo >"$OUTFILE"
+
+# <- END_OF_CODE
+# <- $VAR1 = {
+# <-                VERSION => '0.0.2',
+# <-                purpose => 'email xvn style note',
+# <-                 params => '',
+# <-                example => '',
+# <-                   CODE => $code,
+# <-                 target => "$ENV{HOME}/bin/xn",
+# <- };
+
