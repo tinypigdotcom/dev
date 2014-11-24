d172c01058fefa30b9cf3287ee0e4c59dc4fee7b
David Bradford <dbradford@omniti.com>
2014-11-24 09:33:19 -0500

    fix rebuild switch

--- lf.s.sh
+++ lf.s.sh
@@ -7,10 +7,9 @@ fi
 
 OUTFILE=$HOME/data/full.cat
 
-if [ "$1" == "-r" ]; then
+if [ -n "$u_OPT_REBUILD" ]; then
     echo "Rebuilding index..." >&2
     find /cygdrive >$OUTFILE
-    shift
 fi
 
 grep -i "$*[^/]*$" $OUTFILE | less -FX
@@ -18,11 +17,19 @@ grep -i "$*[^/]*$" $OUTFILE | less -FX
 
 # <- END_OF_CODE
 # <- $VAR1 = {
-# <-     VERSION => '1.1.2',
+# <-     VERSION => '1.1.4',
 # <-     purpose => 'locate file on local machine',
 # <-     params  => 'FILENAME_PART',
 # <-     example => 'menu.h',
 # <-     CODE    => $code,
 # <-     target  => "$ENV{HOME}/bin/lf",
+# <-     options => [
+# <-         {
+# <-             long_switch => 'rebuild',
+# <-             short_desc  => 'rebuild',
+# <-             long_desc   => 'rebuild list of files',
+# <-             init        => '0',
+# <-         },
+# <-     ],
 # <- };
 
