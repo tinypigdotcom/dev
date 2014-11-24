3d31abe38192650ecb70e99a075e35eaee23bb7a
David Bradford <dbradford@omniti.com>
2014-11-23 23:48:49 -0500

    add tt

---
+++ tt.s.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# <- my $code = <<'END_OF_CODE';
+
+task now
+
+# <- END_OF_CODE
+# <- $VAR1 = {
+# <-     VERSION => '0.0.2',
+# <-     purpose => 'show current task warrior tasks',
+# <-     params  => '',
+# <-     example => '',
+# <-     CODE    => $code,
+# <-     target  => "$ENV{HOME}/bin/tt",
+# <- };
+
