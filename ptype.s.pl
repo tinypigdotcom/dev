2dd77d63d21855aa60c355a09261e21db7115770
David Bradford <dbradford@omniti.com>
2014-11-23 23:37:38 -0500

    add ptype

---
+++ ptype.s.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+# <- my $code = <<'END_OF_CODE';
+
+use warnings;
+use strict;
+
+for my $module ( @ARGV ) {
+    my $package = $module;
+
+    # From This::That to This/That.pm
+    s/::/\//g, s/$/.pm/ for $module;
+
+    if ( require $module ) {
+        print $package . " => " . $INC{$module} . "\n";
+        system("vim $INC{$module}") if $Vim;
+    }
+}
+
+# <- END_OF_CODE
+
+# <- $VAR1 = {
+# <-     VERSION => '0.0.2',
+# <-     purpose => 'print full path of a Perl module',
+# <-     example => 'File::Finder',
+# <-     CODE    => $code,
+# <-     target  => "$ENV{HOME}/bin/ptype",
+# <-     options => [
+# <-         {
+# <-             long_switch => 'Vim',
+# <-             short_desc  => 'Vim',
+# <-             long_desc   => "edit module with vim",
+# <-             init        => '0',
+# <-         },
+# <-     ],
+# <- };
+
