21fe73bc0b8d001e1acfbb940b7421aed68ac4b9
David Bradford <dbradford@omniti.com>
2014-11-23 23:47:30 -0500

    add takeit

---
+++ takeit.s.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# <- my $code = <<'END_OF_CODE';
+
+set -x
+cp ~/.vimrc ~/.vim/_vimrc
+cp ~/.bash_profile ~/bin/_bash_profile
+
+# <- END_OF_CODE
+# <- $VAR1 = {
+# <-     VERSION => '0.0.2',
+# <-     purpose => 'to update remote .vimrc and .bash_profile',
+# <-     params  => '',
+# <-     example => '',
+# <-     CODE    => $code,
+# <-     target  => "$ENV{HOME}/bin/takeit",
+# <- };
+
