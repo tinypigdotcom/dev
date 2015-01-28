0f6a9a3d59eb3ff728e62af2a8503bea03b67eb0
David Bradford <dbradford@omniti.com>
2015-01-28 12:16:13 -0500

    cheap clear screen

---
+++ cls.s.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+# <- my $code = <<'END_OF_CODE';
+
+# yes it could be a loop but this should work in multiple shells
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+
+
+# <- END_OF_CODE
+# <- $VAR1 = {
+# <-                VERSION => '0.0.1',
+# <-                   PROG => 'cls',
+# <-                purpose => 'Cheap clear screen since control-l kills scrollback buffer',
+# <-                 params => '',
+# <-                example => '',
+# <-                   exit => 'exit',
+# <-                   CODE => $code,
+# <-                 target => "$ENV{HOME}/bin/cls",
+# <- };
+
