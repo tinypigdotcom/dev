a8af0ec94fdee63a357a1913eeab87b08f0b482b
David Bradford <dbradford@omniti.com>
2015-06-12 13:59:42 -0400

    add perl2json and json2perl

---
+++ json2perl.s.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+# <- my $code = <<'END_OF_CODE';
+
+use Data::Dumper;
+use DMB::Tools ':all';
+use JSON;
+
+$Data::Dumper::Indent=0;
+
+sub main {
+    my @argv = @_;
+    my $json = JSON->new->allow_nonref;
+    my $json_input = do { local $/; <STDIN> };
+    if ( $pretty ) {
+        $Data::Dumper::Indent=2;
+    }
+    my $perlvar = $json->decode( $json_input );
+    print Dumper($perlvar), "\n";
+    return;
+}
+
+my $rc = ( main(@ARGV) || 0 );
+
+exit $rc;
+
+# <- END_OF_CODE
+
+# <- $VAR1 = {
+# <-                VERSION => '0.0.1',
+# <-                purpose => 'convert JSON to perl data structure',
+# <-                 params => '',
+# <-                example => '--pretty',
+# <-                   CODE => $code,
+# <-                 target => "$ENV{HOME}/bin/json2perl",
+# <-     options => [
+# <-         {
+# <-             long_switch => 'pretty',
+# <-              short_desc => 'pretty print',
+# <-               long_desc => "pretty print (versus condensed) the output",
+# <-                    init => '0',
+# <-         },
+# <-     ],
+# <- };
+
---
+++ perl2json.s.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+# <- my $code = <<'END_OF_CODE';
+
+use DMB::Tools ':all';
+use JSON;
+
+sub main {
+    my @argv = @_;
+    my $VAR1;
+    my $json = JSON->new->allow_nonref;
+    my $dumper_output = do { local $/; <STDIN> };
+    eval $dumper_output;
+    if ( $pretty ) {
+        $json = $json->pretty;
+    }
+    my $utf8_encoded_json_text = $json->encode( $VAR1 );
+    print "$utf8_encoded_json_text\n";
+    return;
+}
+
+my $rc = ( main(@ARGV) || 0 );
+
+exit $rc;
+
+# <- END_OF_CODE
+
+# <- $VAR1 = {
+# <-                VERSION => '0.0.1',
+# <-                purpose => 'convert perl Dumper data structure to JSON',
+# <-                 params => '',
+# <-                example => '--pretty',
+# <-                   CODE => $code,
+# <-                 target => "$ENV{HOME}/bin/perl2json",
+# <-     options => [
+# <-         {
+# <-             long_switch => 'pretty',
+# <-              short_desc => 'pretty print',
+# <-               long_desc => "pretty print (versus condensed) the output",
+# <-                    init => '0',
+# <-         },
+# <-     ],
+# <- };
+
