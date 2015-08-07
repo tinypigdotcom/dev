70314210ceb2bbac4072f13c73ad7103718f8671
David Bradford <dbradford@omniti.com>
2015-08-07 10:45:19 -0400

    use multi_input

--- percent_encode.s.pl
+++ percent_encode.s.pl
@@ -7,7 +7,7 @@ use URI::Escape;
 sub main {
     my @argv = @_;
     my $VAR1;
-    my $input = do { local $/; <STDIN> };
+    my $input = multi_input();
     eval $input;
     my $uri_encoded_text = uri_escape_utf8( $input );
     print "$uri_encoded_text\n";
@@ -21,7 +21,7 @@ exit $rc;
 # <- END_OF_CODE
 
 # <- $VAR1 = {
-# <-                VERSION => '0.0.1',
+# <-                VERSION => '0.0.2',
 # <-                purpose => 'percent encode text for URLs',
 # <-                 params => '',
 # <-                example => '',
