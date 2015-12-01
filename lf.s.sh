7ca804403b5da4a9101f6fd0b7bafac640dfe9db
David Bradford <dbradford@omniti.com>
2015-12-01 14:04:59 -0500

    fix order of processing options

--- lf.s.sh
+++ lf.s.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 # <- my $code = <<'END_OF_CODE';
 
-if [ "$#" -lt 1 ]; then
-    u_do_short_usage
-fi
-
 OUTFILE=$HOME/data/full.cat
 
 if [ -n "$u_opt_r$u_opt_rebuild" ]; then
     echo "Rebuilding index..." >&2
     find /cygdrive >$OUTFILE
-else
+elif [ "$#" -lt 1 ]; then
+    u_do_short_usage
+fi
+
+if (($#)); then
     grep -i "$*[^/]*$" $OUTFILE | less -FX
 fi
 
 # <- END_OF_CODE
 # <- $VAR1 = {
-# <-     VERSION => '1.1.6',
+# <-     VERSION => '1.1.7',
 # <-     purpose => 'locate file on local machine',
 # <-     params  => 'FILENAME_PART',
 # <-     example => 'menu.h',
